ecc2807c16f4e6b810932a4a3d706f5722c600d7
negretenico <negretenico0@gmail.com>
2023-09-11 21:22:01 -0400

    Adding simple player loading image

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from player.player import Player
 import pygame 
 import sys 
-from typing import Final 
+from typing import Final
+
+from utils.point import Point 
 SCREEN_WIDTH: Final = 1280
 SCREEN_HEIGHT: Final = 720 
 
@@ -9,8 +12,12 @@ pygame.init()
 bg = pygame.image.load("assets\images\\back_ground.png")
 bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+inital_point = Point(SCREEN_WIDTH//2,SCREEN_HEIGHT//2)
+idle = 'assets\\images\\Zetterburn\\zet_idle_new\\zet_idle_new_0.png'
+player = Player(position=inital_point,image=idle)
 
 while True:
     clock.tick(60)
     screen.blit(bg,(0,0))
+    screen.blit(player.get_sprite_image(),player.get_position())
     pygame.display.update()
