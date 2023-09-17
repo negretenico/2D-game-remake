410e9a4f7b49079c3cc0658b7b2ca30ae810209e
negretenico <negretenico0@gmail.com>
2023-09-16 22:59:34 -0400

    Adding new tests

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -2,9 +2,12 @@ import unittest
 from src.player.player import Player
 from typing import Dict
 from src.utils.key_bindings import KeyBindings
+from unittest.mock import Mock, patch
+from pygame import Surface
 class TestUtils(unittest.TestCase):
-    def test_can_get(self):
-        key_bindings:Dict[int,callable] = KeyBindings(player=Player(None,None))
+    @patch("pygame.image.load",return_value=Surface((1,1)))
+    def test_can_get(self,mock_load):
+        key_bindings:Dict[int,callable] = KeyBindings(player=Player(None,None)).get_key_bindings()
         self.assertTrue(len(key_bindings)!=0)
 
 if __name__ =="__main__":
